b821939abfdbfee0b95a34a376473e6698417428
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-10 15:46:03 +0000

    Using GetBuilder widget to listening to HomeController & rebuild UI

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:learn_getx_app/view/page_one_view.dart';
-import 'package:learn_getx_app/view/page_three_view.dart';
-import 'package:learn_getx_app/view/page_two_view.dart';
+import 'package:learn_getx_app/controller/home_controller.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -19,28 +17,36 @@ class HomeView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add),
-                ),
-                const SizedBox(width: 25),
-                Text(
-                  '0',
-                  style: const TextStyle(
-                    fontSize: 28,
-                    color: Colors.red,
-                  ),
-                ),
-                const SizedBox(width: 25),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove),
-                ),
-              ],
-            ),
+            GetBuilder<HomeController>(
+                init: HomeController(),
+                builder: (controller) {
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          controller.increment();
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                      const SizedBox(width: 25),
+                      Text(
+                        '${controller.counter}',
+                        style: const TextStyle(
+                          fontSize: 28,
+                          color: Colors.red,
+                        ),
+                      ),
+                      const SizedBox(width: 25),
+                      IconButton(
+                        onPressed: () {
+                          controller.decrement();
+                        },
+                        icon: const Icon(Icons.remove),
+                      ),
+                    ],
+                  );
+                }),
           ],
         ),
       ),
