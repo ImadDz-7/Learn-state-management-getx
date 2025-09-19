26926ecb834f6637bd01f6fb99d9781bbf483e02
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-19 11:31:47 +0000

    Test bindings

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learn_getx_app/controller/home_controller.dart';
+import 'package:learn_getx_app/utils/my_bindings.dart';
 import 'package:learn_getx_app/view/page_four_view.dart';
 import 'package:learn_getx_app/view/page_one_view.dart';
 import 'package:learn_getx_app/view/page_three_view.dart';
@@ -9,7 +10,7 @@ import 'package:learn_getx_app/view/page_two_view.dart';
 class HomeView extends StatelessWidget {
   HomeView({super.key});
 
-  final controller = Get.lazyPut(() => HomeController(), fenix: true);
+  // final controller = Get.lazyPut(() => HomeController(), fenix: true);
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +34,7 @@ class HomeView extends StatelessWidget {
             const SizedBox(height: 25),
             MaterialButton(
               onPressed: () {
-                Get.to(() => PageTwoView());
+                Get.to(() => PageTwoView(), binding: MyBindings());
               },
               color: Colors.red,
               child: const Text('Page Two'),
