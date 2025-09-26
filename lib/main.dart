b8689b6b0ffec10f750ae7c36dda96498a9d064a
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-09-26 11:25:12 +0000

    Adding initialServices method below the main method

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learn_getx_app/middleware/auth_middleware.dart';
 import 'package:learn_getx_app/middleware/super_middleware.dart';
+import 'package:learn_getx_app/services/settings_services.dart';
 import 'package:learn_getx_app/utils/my_bindings.dart';
 import 'package:learn_getx_app/view/admin_view.dart';
 import 'package:learn_getx_app/view/home_view.dart';
@@ -15,6 +16,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharepref = await SharedPreferences.getInstance();
   runApp(const LearnGetxApp());
+}
+
+Future initialServices() async{
+  await Get.putAsync(() => SettingsServices().init());
 }
 
 class LearnGetxApp extends StatelessWidget {
